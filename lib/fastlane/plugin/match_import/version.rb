aa4a475e73c0335d7e0b9b47f7748c139c821c70
Serhii Batsevych <serhii.batsevych@skelia.com.ua>
2020-04-06 15:22:26 +0300

    updated

--- lib/fastlane/plugin/match_import/version.rb
+++ lib/fastlane/plugin/match_import/version.rb
@@ -1,6 +1,6 @@
 module Fastlane
   module MatchImport
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
     DESCRIPTION = "match_import command import/export files to/from match encrypted repo. Also remove files is supported"
   end
 end
